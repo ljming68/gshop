2555876a58b5d7df695270d5307a4a666e59a36d
LJMing <376855074@qq.com>
2021-10-24 11:49:17 +0800

    init app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <First></First>
+  </div>
+</template>
+
+<script>
+import First from './components/first'
+export default {
+  components:{
+    First
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
